e989d3a071021244113d5f0ba802892a627527c9
dai.zhihong <1106869711@qq.com>
2022-01-29 02:21:13 +0800

    modify mapping

--- src/main/java/com/example/springmybatis/mapper/UserMapper.java
+++ src/main/java/com/example/springmybatis/mapper/UserMapper.java
@@ -3,8 +3,12 @@ package com.example.springmybatis.mapper;
 import com.example.springmybatis.entity.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserMapper {
 
     User Sel(int id);
+
+    List<User> findByRoleCode(String roleCode);
 }
